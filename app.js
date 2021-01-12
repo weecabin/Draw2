eef0267d2ce5bbf856636f3606e7e9db3acab5a2
play.js <git@gituser.com>
2021-01-12 01:21:43 -0800

    added status line for debug

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ class Draw()
 {
   constructor(canvasId)
   {
+    AddStatus("entering constructor")
     this.c = document.getElementById(canvasId);
     this.ctx = c.getContext("2d");
+    AddStatus("Exiting constructor")
   }
   
   function line(from,to)
   {
+    AddStatus("entering line")
     this.ctx.moveTo(from[0],from[1]);
     this.ctx.lineTo(to[0],to[1]);
     this.ctx.stroke();
+    AddStatus("exiting line")
   }
 }
 
@@ -22,3 +26,7 @@ function setup()
   d.line([100,100],[150,50]);
 }
 
+function AddStatus(str)
+{
+  document.getElementById("status").value+="\n"+str
+}
