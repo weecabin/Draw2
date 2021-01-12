5f622de2b10c9a0cf13f78250bfd7264bdde9f58
play.js <git@gituser.com>
2021-01-12 10:48:25 -0800

    log plot

--- app.js
+++ app.js
@@ -10,11 +10,22 @@ class Draw
     AddStatus("Exiting constructor")
   }
   
-  line(from,to)
+  Line(from,to)
   {
     AddStatus("line from "+from+" to "+to)
     this.ctx.moveTo(from[0],from[1]);
     this.ctx.lineTo(to[0],to[1]);
+    this.ctx.stroke();
+  }
+  /*
+  draws a line from as defined by the double array of points
+  [[x1,y1],[x2,y2]...]
+  */
+  Path(points)
+  {
+    this.ctx.moveTo(points[0],points[1]);
+    for(i=1;i<points.length;i++)
+      this.ctx.lineTo(to[0],to[1]);
     this.ctx.stroke();
   }
 }
@@ -23,13 +34,28 @@ function setup()
 {
   document.getElementById("status").value="form load complete.";
   var d = new Draw("myCanvas");
-  d.line([0,0],[300,300]);
-  d.line([300,300],[300,50]);
+  d.Line([0,0],[300,300]);
+  d.Line([300,300],[300,50]);
   
-  d.line([300,290],[290,300]);
-  d.line([290,300],[300,310]);
-  d.line([300,310],[310,300]);
-  d.line([310,300],[300,290]);
+  d.Line([300,290],[290,300]);
+  d.Line([290,300],[300,310]);
+  d.Line([300,310],[310,300]);
+  d.Line([310,300],[300,290]);
+  
+  var points = [];
+  for (x=0;x<600;x+=20)
+  {
+    let y=Math.round(Math.power(i,2)/600)
+    points.push([x,y])
+  }
+  d.Path(points)
+}
+
+function circle(centerX,centerY,radius)
+{
+  // r^2 = (x-a)^2 + (y-b)^2
+  //y=Math.sqrt(r^2-(x-a)^2)+b
+  
 }
 
 function AddStatus(str)
