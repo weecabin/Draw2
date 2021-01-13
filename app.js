7d67ad729d795870f2cb087d9f19f6209bc29808
play.js <git@gituser.com>
2021-01-13 11:06:17 -0800

    090

--- app.js
+++ app.js
@@ -78,11 +78,11 @@ ReDraw()
   this.ctx.save();
 
   // Use the identity matrix while clearing the canvas
-  context.setTransform(1, 0, 0, 1, 0, 0);
-  context.clearRect(0, 0, canvas.width, canvas.height);
+  this.ctx.setTransform(1, 0, 0, 1, 0, 0);
+  this.ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   // Restore the transform
-  context.restore();
+  this.ctx.restore();
   
   AddStatus(this.dataset)
   try
